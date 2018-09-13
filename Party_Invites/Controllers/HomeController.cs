52ae47926eeb01261b177c296a2cd44342b96879
Ahmed Jamal <Ahmed Jamal@LAPTOP-CQULRIFS>
2018-09-13 23:19:30 +0300

    Add home page and build model only

---
+++ Party_Invites/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Party_Invites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int Hours = DateTime.Now.Hour;
+            ViewBag.Greeting=Hours<12?"Good Morning":"Good Afternoon";
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ Party_Invites/Models/GestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Party_Invites.Models
+{
+    public class GestResponse
+    {
+        public string Name { set; get; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
